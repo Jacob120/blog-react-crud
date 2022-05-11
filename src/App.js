4f5e8f6d401c7de376fa55e38eb35b0186e52d9d
Jakub Kozik <jakubkozik120@gmail.com>
2022-05-11 20:19:47 +0200

    Add views > Footer / Header / NavBar

--- src/App.js
+++ src/App.js
@@ -5,21 +5,28 @@ import Post from './components/pages/Post';
 import PostAdd from './components/pages/PostAdd';
 import PostEdit from './components/pages/PostEdit';
 import { Routes, Route} from 'react-router-dom';
+import Container from 'react-bootstrap/Container';
+import Header from './components/views/Header';
+import Footer from './components/views/Footer';
 
 const  App = () => {
   return (
     <main>
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/about' element={<About />} />
-        <Route path='/post/:postId' element={<Post />} />
-        <Route path='/post/add' element={<PostAdd />} />
-        <Route path='/post/edit' element={<PostEdit />} />
-        <Route
-            path="*"
-            element={<NotFound />}
-          />
-      </Routes>
+      <Container>
+        <Header />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/post/:postId' element={<Post />} />
+          <Route path='/post/add' element={<PostAdd />} />
+          <Route path='/post/edit' element={<PostEdit />} />
+          <Route
+              path="*"
+              element={<NotFound />}
+            />
+        </Routes>
+        <Footer />
+      </Container>
     </main>
   );
 }
---
+++ src/components/views/Footer.js
@@ -0,0 +1,11 @@
+import Card from 'react-bootstrap/Card';
+
+const Footer = () => {
+  return ( 
+    <Card>
+      <Card.Footer className="text-muted text-center">Copyright &copy; BlogApp 2022</Card.Footer>    
+    </Card>   
+  );
+}
+
+export default Footer;
