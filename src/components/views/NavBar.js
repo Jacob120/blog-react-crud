50093e3e9da27bd9516af0021b57ac63806d6263
Jakub Kozik <jakubkozik120@gmail.com>
2022-05-19 15:19:08 +0200

    Add hamburger menu

--- src/components/views/NavBar.js
+++ src/components/views/NavBar.js
@@ -6,14 +6,17 @@ import { NavLink } from 'react-router-dom';
 
 const NavBar = () => {
   return (
-   <Navbar bg="primary" variant="dark" expand="lg" className="mt-4 mb-4 rounded">
+   <Navbar collapseOnSelect bg="primary" variant="dark" expand="lg" className="mt-4 mb-4 rounded">
     <Container>
       <Navbar.Brand href="/">Blog.app</Navbar.Brand>
-      <Nav className="me-right">
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse id="responsive-navbar-nav">
+      <Nav className="justify-content-end flex-grow-1 pe-3">
         <Nav.Link as={NavLink} to="/">Home</Nav.Link>
         <Nav.Link as={NavLink} to="/categories">Categories</Nav.Link>
         <Nav.Link as={NavLink} to="/about">About</Nav.Link>
       </Nav>
+      </Navbar.Collapse>
     </Container>
    </Navbar>
   );
