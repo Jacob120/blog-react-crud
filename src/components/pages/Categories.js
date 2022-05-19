dd02f8d7838ee6feca57e435a389ca42f77f3534
Jakub Kozik <jakubkozik120@gmail.com>
2022-05-19 15:14:22 +0200

    Fix Categories link RWD

--- src/components/pages/Categories.js
+++ src/components/pages/Categories.js
@@ -1,6 +1,8 @@
 import { getAllCategories } from '../../redux/categoriesRedux';
 import { useSelector } from 'react-redux';
 import Button from 'react-bootstrap/Button';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import { Link } from 'react-router-dom';
 
 const Categories = () => {
@@ -10,9 +12,13 @@ const Categories = () => {
   return (
     <section>
       <h1 className="mb-5">All categories</h1>
-      {categories.map(category => (
-        <Link to={"/category/" + category} key={category} className="m-3"><Button variant="primary" className="px-5">{category}</Button></Link>
-      ))}
+      <Row xs={1} md={5} className="g-3 justify-content-md-center" >
+          {categories.map(category => (
+        <Col >
+            <Link to={"/category/" + category} key={category} className="m-3"><Button variant="primary"  className="px-5">{category}</Button></Link>
+        </Col>
+          ))}
+      </Row>
     </section>
   )
 
